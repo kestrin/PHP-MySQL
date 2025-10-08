487c4005d8428062eaf54fd98c9f82fea93907bc
kestrin <kesta1234455@gmail.com>
2025-10-08 20:46:22 +0200

    Create multidimensionalArrays.php

---
+++ Module_5/multidimensionalArrays.php
@@ -0,0 +1,12 @@
+<?php
+    $dogs = array(
+    array("Doberman","Mexico",20),
+    array("Husky","Siberia",15),
+    array("Bulldog","England",10),
+    );
+    
+    echo $dogs[0][0].":Origin:" .$dogs[0][1].":Life span:".$dogs[0][2].".<br>";
+    echo $dogs[1][0].":Origin:" .$dogs[1][1].":Life span:".$dogs[1][2].".<br>";
+    echo $dogs[2][0].":Origin:" .$dogs[2][1].":Life span:".$dogs[2][2].".<br>";
+
+?>  
