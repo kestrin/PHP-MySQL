62f27675b8fb59128e95c3ecb197cd5285706cbb
kestrin <kesta1234455@gmail.com>
2025-11-10 20:46:53 +0100

    delete and edit methods

---
+++ Module_11/edit.php
@@ -0,0 +1,15 @@
+<?php
+include_once('config.php');
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM users WHERE id = :id";
+
+$prep = $conn->prepare($sql);
+
+$prep->bindParam(':id', $id);
+
+$prep->execute();
+
+$data = $prep->fetch();
+?>
